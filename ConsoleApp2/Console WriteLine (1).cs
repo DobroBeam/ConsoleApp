0f268c24b62676c59aca3898717a3123ef4e641d
Dmitry Lazarenko <dobrobeam@gmail.com>
2021-05-30 20:52:26 +0300

    1) Rename "Program.cs" to "Console WriteLine (1).cs" 2) Edit "Console WriteLine (1).cs"

---
+++ ConsoleApp2/Console WriteLine (1).cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string MyName = "Dmitry";
+            byte MyAge = 27;
+            bool DoIHaveAPet = false;
+            float MyShoeSize = 41;
+            Console.WriteLine($"My name is {MyName}\nI am {MyAge}\nDo I have a pet? {DoIHaveAPet}\nMy shoe size is {MyShoeSize}");
+            Console.ReadKey();
+        }
+    }
+}
