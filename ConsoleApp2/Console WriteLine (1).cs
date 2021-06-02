7c8c10a0827f606f33e5cf1422139fcd396b14bb
Dmitry Lazarenko <dobrobeam@gmail.com>
2021-06-02 15:10:47 +0300

    добавлен новый класс для модуля 3.7

--- ConsoleApp2/Console WriteLine (1).cs
+++ ConsoleApp2/Console WriteLine (1).cs
@@ -11,7 +11,10 @@ namespace ConsoleApp2
             bool DoIHaveAPet = false;
             float MyShoeSize = 41;
             Console.WriteLine($"My name is {MyName}\nI am {MyAge}\nDo I have a pet? {DoIHaveAPet}\nMy shoe size is {MyShoeSize}");
+            Console.WriteLine($"Byte max value {byte.MaxValue} \nByte min value {byte.MinValue}");
+            Console.WriteLine($"Long max value {long.MaxValue} \nLong min value {long.MinValue}");
+            Console.WriteLine($"Ushort max value {ushort.MaxValue} \nUshort min value {ushort.MinValue}");
             Console.ReadKey();
         }
-    }
+    }   
 }
