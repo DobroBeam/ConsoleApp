cd4f2af029925e3ced1161f9e56a95aba9eaaedc
Dmitry Lazarenko <dobrobeam@gmail.com>
2021-06-08 21:05:57 +0300

    4.2.9. Результат работы программы для цикла for

--- 4.1.testSwitch/testSwitch.cs
+++ 4.1.testSwitch/testSwitch.cs
@@ -7,32 +7,38 @@ namespace _4._1.testSwitch
         static void Main(string[] args)
         {
             Console.Write("Введи на английском свой любимый цвет (со строчной буквы):");
-            var color = Console.ReadLine();
-
-            switch(color)
+            
+           for (int i=0; i<3;i++)
             {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine ("Ваш любимый цвет красный!");
-                    break;
+                var color = Console.ReadLine();
+                switch (color)
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Ваш любимый цвет красный!");
+                        break;
 
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Ваш любимый цвет зелёный!");
-                    break;
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Ваш любимый цвет бирюзовый!");
-                    break;
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Ваш любимый цвет жёлтый!");
-                    break;                    
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Ваш любимый цвет зелёный!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Ваш любимый цвет бирюзовый!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Ваш любимый цвет жёлтый!");
+                        break;
+                }
+                Console.WriteLine(i);
             }
+
+
             Console.ReadKey();
         }
     }
