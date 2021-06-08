917060c41bb65f53260f7fc950d9a554408fc57c
Dmitry Lazarenko <dobrobeam@gmail.com>
2021-06-08 23:39:53 +0300

    4.2.15. Программа складывающая положительные числа

---
+++ 4.2.circles/testCircles.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _4._2.circles
+{
+    class testCircles
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            while (true)
+            {
+                Console.Write("Введите число: ");
+                var number = Convert.ToInt32(Console.ReadLine());
+                if (number == 0)
+                {
+                    break;
+                }
+                else if (number < 0) 
+                {
+                    continue;
+                }
+                sum += number;
+            }
+            Console.WriteLine("Программа остановлена\nИтоговая сумма: {0}", sum);
+        }
+    }
+}
