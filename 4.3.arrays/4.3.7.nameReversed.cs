5dc5c61ceebfeb6ea19483c47c8787ec5c77f09b
Dmitry Lazarenko <dobrobeam@gmail.com>
2021-06-09 16:53:22 +0300

    4.3.7. вывод имени посимвольно в обратном порядке

---
+++ 4.3.arrays/4.3.7.nameReversed.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._3.arrays
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите своё имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Имя в обратном порядке: ");
+
+            for (var i=name.Length; i > 0; i--)
+            {
+               {
+                    Console.Write(name[i - 1]);
+               }
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
