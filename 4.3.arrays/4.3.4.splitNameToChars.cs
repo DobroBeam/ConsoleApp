a4febf352c612f0df904a756254475480b98632c
Dmitry Lazarenko <dobrobeam@gmail.com>
2021-06-09 14:17:04 +0300

    4.3.4. Разделить свое имя по буквам + добавить последнюю букву имени

---
+++ 4.3.arrays/4.3.4.splitNameToChars.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._3.arrays
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите своё имя: ");
+            string name = Console.ReadLine();
+            int nameLength = name.Length;
+            Console.Write("Ваше имя по буквам: ");
+
+            foreach (char symbol in name)
+            {
+                Console.Write("{0} ", symbol);
+            }
+
+
+            for (int i=0; i != name.Length+1; i++)
+            {
+                if (i == name.Length)
+                {
+                    Console.WriteLine("\nПоследняя буква имени: {0}", name[i - 1]);
+                }
+            }
+
+
+        }
+    }
+}
