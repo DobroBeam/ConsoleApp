99572ba5f81c81fe7c8b1125e8cabbf44ac62b49
Dmitry Lazarenko <dobrobeam@gmail.com>
2021-06-10 12:16:34 +0300

    4.3.13. Сортировка двумерного массива по возрастанию (свой вариант)

---
+++ 4.3.arrays/4.3.13.arraySorting.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._3.arrays
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            // Задание 4.3.13. Сортировка по возрастанию двумерного массива
+
+            var array = new int[] { 2,3,1,9,0,5,8,8,8,11,101,99 }; // 2-мерный массив для сортировки
+            int minValue = int.MaxValue; // переменная для хранения текущего минимального значения при обходе массива
+            var minIndex = array.Length + 1; // переменная для хранения индекса минимального значения
+            var firstChar = array[0]; // переменная для хранения замещаемого значения
+            
+            for (int k = 0; k < array.Length; k++) // обход массива и обмен значений k-го элемента и элемента с минимальным значением
+            {
+                    for (int i = k; i < array.Length; i++) //ищем индекс минимального значения в массиве
+                    {
+                        if (array[i] < minValue)
+                        {
+                            minValue = array[i];
+                            minIndex = i;
+                        }
+                    }
+                firstChar = array[k]; // cохраняем значение замещаемого элемента в firstChar
+                array[k] = array[minIndex]; // сохраняем в замещаемый элемент минимальное значение
+                array[minIndex] = firstChar; // сохраняем значение замещенного элемента в элемент с минимальным значением
+                minValue = int.MaxValue; // сбрасываем текущее минимальное значение на исходное
+
+                Console.Write(array[k] + " "); // вывод элемента с минимальным значением
+            }
+            Console.ReadKey();
+        }
+    }
+}
