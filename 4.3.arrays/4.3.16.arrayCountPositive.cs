07e0f07cbbd1d7523a1f98249060a4537d1ec3ad
Dmitry Lazarenko <dobrobeam@gmail.com>
2021-06-11 13:56:49 +0300

    4.3.17. Сортировка массива построчно

--- 4.3.arrays/4.3.16.arrayCountPositive.cs
+++ 4.3.arrays/4.3.16.arrayCountPositive.cs
@@ -24,7 +24,9 @@ namespace _4._3.arrays
                 }
             }
             Console.WriteLine(sum1);
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Нажмите, чтобы продолжить\n");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.ReadKey();
 
             Console.WriteLine("Сумма всех положительных элементов массива (цикл for)");
@@ -41,7 +43,38 @@ namespace _4._3.arrays
                 }
                 Console.WriteLine("Строка {0}: {1}", i+1, sum1);
             }
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Нажмите, чтобы продолжить\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+
+            // Задание 4.3.17. Сортировка элементов массива внутри одной строки.
+            Console.WriteLine("Сортировка массива построчно");
+
+            int firstChar; // переменная для хранения замещаемого значения
+                        
+            for (int i = 0; i < array.GetUpperBound(0) + 1; i++) // цикл входа в строку
+            {
+                Console.WriteLine("Строка {0}: ", i+1);
+
+                for (int k = 0; k < array.GetUpperBound(1) + 1; k++) // цикл сортировки элементов в строке по возрастанию
+                {
+                    for (int t = k + 1; t < array.GetUpperBound(1) + 1; t++)
+                    {
+                        if (array[i, k] > array[i, t])
+                        {
+                            firstChar = array[i, k];
+                            array[i, k] = array[i, t];
+                            array[i, t] = firstChar;
+                        }
+                    }
+                    Console.Write($"{array[i, k]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Нажмите, чтобы продолжить\n");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.ReadKey();
         }
     }
