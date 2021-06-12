77894bd3971458e00e1c946954c456d1a190fa28
Dmitry Lazarenko <dobrobeam@gmail.com>
2021-06-12 13:07:15 +0300

    4.4.5. заполнение кортежа данными

--- 4.4.tuples/4.4.2.surveyViaTuple.cs
+++ 4.4.tuples/4.4.2.surveyViaTuple.cs
@@ -6,6 +6,7 @@ namespace _4._4.tuples
     {
         static void Main(string[] args)
         {
+            // Задание 4.4.2. Модифицируйте свою программу для ввода личной информации пользователя так, чтобы данные записывались в кортеж anketa
             (string name, string surname, int age) anketa;
             Console.Write("Введите ваше имя: ");
             anketa.name = Console.ReadLine();
@@ -16,10 +17,26 @@ namespace _4._4.tuples
             
             Console.WriteLine("Ваше имя: {0}\nВаша фамилия: {1}\nВаш возраст: {2}", anketa.name, anketa.surname, anketa.age);
 
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Нажмите, чтобы продолжить\n");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.ReadKey();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Нажмите, чтобы продолжить\n");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadKey();
+
+            // Задание 4.4.3. Перезаписать данные в кортеже.
+            var (name2, surname2, age2) = ("Евгения", "Иванова", 27);
+            Console.WriteLine("Ваше имя: {0}\nВаша фамилия: {1}\nВаш возраст: {2}", name2, surname2, age2);
+            Console.Write("Введите ваше имя: ");
+            name2 = Console.ReadLine();
+            Console.Write("Введите вашу фамилию: ");
+            surname2 = Console.ReadLine();
+            Console.Write("Введите ваш возраст: ");
+            age2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваше имя: {0}\nВаша фамилия: {1}\nВаш возраст: {2}", name2, surname2, age2);
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Нажмите, чтобы продолжить\n");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadKey();
         }
     }
 }
---
+++ 4.4.tuples/4.4.5.surveyPetTuple.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _4._4.tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 4.4.5. Дан кортеж который нужно заполнить
+            //Name — имя питомца;
+            //Type — тип питомца: собака, рыбка, хомячок;
+            //Age — возраст питомца;
+            //NameCount — длина имени питомца.
+
+            (string name, string type, double age, int NameCount) Pet; // заданный кортеж
+
+            Console.Write("Введите имя питомца: ");
+            Pet.name = Console.ReadLine();
+            Console.Write("Кто ваш питомец: ");
+            Pet.type = Console.ReadLine();
+            Console.Write("Сколько лет питомцу: ");
+            Pet.age = Convert.ToDouble(Console.ReadLine());
+            Pet.NameCount = Convert.ToInt32(Pet.name.Length);
+            Console.WriteLine("Имя питомца: {0}\nВаш питомец: {1}\nЕго возраст: {2}\nДлина имени: {3}", Pet.name, Pet.type, Pet.age, Pet.NameCount);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Нажмите, чтобы продолжить\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+        }
+    }
+}
