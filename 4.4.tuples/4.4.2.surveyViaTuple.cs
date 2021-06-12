05ef2930d2f234dd9fadeaeec77747d6b2a7179f
Dmitry Lazarenko <dobrobeam@gmail.com>
2021-06-12 11:56:54 +0300

    4.4.2.  программа для ввода личной информации (данные записываются в кортеж anketa)

---
+++ 4.4.tuples/4.4.2.surveyViaTuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _4._4.tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string name, string surname, int age) anketa;
+            Console.Write("Введите ваше имя: ");
+            anketa.name = Console.ReadLine();
+            Console.Write("Введите вашу фамилию: ");
+            anketa.surname = Console.ReadLine();
+            Console.Write("Введите ваш возраст: ");
+            anketa.age = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Ваше имя: {0}\nВаша фамилия: {1}\nВаш возраст: {2}", anketa.name, anketa.surname, anketa.age);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Нажмите, чтобы продолжить\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+        }
+    }
+}
