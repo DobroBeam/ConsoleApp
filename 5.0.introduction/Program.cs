4c4b160eae6a58568912007f88ff72a2500d9eb1
Dmitry Lazarenko <dobrobeam@gmail.com>
2021-06-13 19:09:08 +0300

    5.1.5. Напишите программу, которая в цикле вызывает метод ShowColor(), записывает его значение в массив из трех цветов favcolors, а потом отображает значения этого массива.

---
+++ 5.0.introduction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _5._0.introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string name, string[] dishes) User;
+            Console.WriteLine("Введите ваше имя: ");
+            User.name = Console.ReadLine();
+            User.dishes = new string[5];
+            Console.WriteLine("Введите ваши любимые блюда: ");
+            for (int i = 0; i < User.dishes.Length; i++)
+            {
+                Console.Write($"Блюдо {i+1} :");
+                User.dishes[i] = Console.ReadLine();
+            }
+            Console.WriteLine($"Ваше имя: {User.name}");
+            Console.Write("Ваши любимые блюда: ");
+            foreach (var item in User.dishes)
+            {
+                Console.Write($"{item} ");
+            }
+
+        }
+    }
+}
---
+++ 5.1.methodCall/5.1.5.methodCallPractice.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _5._1.methodCall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] favcolors;
+            favcolors = new string[3];
+            for (int i=0; i < 3; i++)
+            {
+                favcolors[i] = ShowColor();
+                Console.WriteLine("Принято");
+            }
+            Console.WriteLine("\nСписок ваших любимых цветов:");
+            foreach (var item in favcolors)
+            { 
+                Console.Write($"{item} ");
+            }
+            ClickToProceed();
+        }
+        static string ShowColor()
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском маленькими буквами: ");
+            var color = Console.ReadLine();
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is red!");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case "yellow":
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is yellow!");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is green!");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is cyan!");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+            return color;
+        }
+        static void ClickToProceed()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nНажмите, чтобы продолжить\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+        }
+    }
+}
