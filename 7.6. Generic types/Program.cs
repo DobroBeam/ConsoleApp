2ce33280eb4b475ae5838b4b8a897ba2433ca2ec
Dmitry Lazarenko <dobrobeam@gmail.com>
2022-04-20 10:01:36 +0300

    7.6.2

--- 7.6. Generic types/Program.cs
+++ 7.6. Generic types/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace _7._6._Generic_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+// Задание 7.6.2
+// Создайте класс-обобщение Car для автомобиля.Универсальным параметром будет тип двигателя в автомобиле (электрический и бензиновый).
+// Для типов двигателей также создайте классы — ElectricEngine и GasEngine.
+// В классе Car создайте поле Engine в качестве типа которому укажите универсальный параметр.
+    class Car<T>
+    {
+        public T Engine;
+    }
+    class ElectricEngine { }
+    class GasEngine { }
+}
