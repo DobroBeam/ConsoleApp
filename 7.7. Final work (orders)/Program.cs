5d92075ec9f23ecb6461d4ee3dde11a88ed2ecb8
Dmitriy Lazarenko <fondango@inbox.ru>
2022-04-20 22:31:43 +0300

    7.7. начало создания дерева классов

---
+++ 7.7. Final work (orders)/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._7.Final_work__orders_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Customer 
+    {
+        public string firstName;
+        public string lastName;
+        public int phoneNumber;
+        public string eMail;
+        public string deliveryAddress;
+    }
+    class Vendor 
+    {
+        public string vendorName;
+    }
+    class Individual : Vendor 
+    {
+        public int passport;
+    }
+    class Entity : Vendor 
+    {
+        public int taxNumber;
+    }
+    class Good { }
+    class Order<TDelivery> 
+    {
+        public TDelivery Delivery;
+
+    }
+    class Delivery 
+    {
+        public string address;
+    }
+    class HomeDelivery : Delivery
+    {}
+
+    class PickPointDelivery : Delivery
+    {}
+
+    class ShopDelivery : Delivery
+    {}
+
+
+
+}
