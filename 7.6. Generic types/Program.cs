9d54be2f3a1476226308b607e36a01d771b9653f
Dmitry Lazarenko <dobrobeam@gmail.com>
2022-04-20 13:13:38 +0300

    7.6.7

--- 7.6. Generic types/Program.cs
+++ 7.6. Generic types/Program.cs
@@ -6,20 +6,45 @@ namespace _7._6._Generic_types
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Obj obj = new Obj(); //создаем объект типа Obj
+            obj.Display<int>(345); //передаем в метод Display значение 345, которое методу нужно воспринять как значение типа Int
+            int num1 = 4;
+            int num2 = 10;
+            Swap<int>(ref num1, ref num2); // вызываем метод Swap передаем ему тип данных с которым ему надо работать и передаем по ссылке 2 параметра
+            Console.WriteLine($"{num1}, {num2}");
+            Console.ReadKey();
+
+        }
+        public static void Swap<T>(ref T x, ref T y) // создаем универсальный метод Swap (на универсальность указывает <T> после названия), который принимает 2 параметра типа данных T по ссылке
+        {
+            T t=x; // создаем переменную t типа T (переданного в метод) и присваиваем ей значение x
+            x=y;
+            y=t;
         }
 
     }
-// Задание 7.6.2
-// Создайте класс-обобщение Car для автомобиля.Универсальным параметром будет тип двигателя в автомобиле (электрический и бензиновый).
-// Для типов двигателей также создайте классы — ElectricEngine и GasEngine.
-// В классе Car создайте поле Engine в качестве типа которому укажите универсальный параметр.
-    class Car<T>
+    // Задание 7.6.2
+    // Создайте класс-обобщение Car для автомобиля.Универсальным параметром будет тип двигателя в автомобиле (электрический и бензиновый).
+    // Для типов двигателей также создайте классы — ElectricEngine и GasEngine.
+    // В классе Car создайте поле Engine в качестве типа которому укажите универсальный параметр.
+
+    //  Задание 7.6.7
+    //  Добавьте к схеме классов автомобиля следующие классы частей автомобиля: Battery, Differential, Wheel.
+    //  Также добавьте в класс Car виртуальный обобщённый метод без реализации — ChangePart,
+    //  который будет принимать один параметр — newPart универсального типа.
+    class Car<T1>
     {
-        public T Engine;
+        public T1 Engine;
+        public virtual void ChangePart<T2>(T2 newPart)
+        {
+        
+        }
     }
     class ElectricEngine { }
     class GasEngine { }
+    class Battery { }
+    class Differential { }
+    class Wheel { }
 
 //  Задание 7.6.6
 //  Реализуйте класс-обобщение Record, у которого будут два универсальных параметра:
@@ -29,5 +54,12 @@ namespace _7._6._Generic_types
         public T1 Id;
         public T2 Value;
         public DateTime Date;
+    }
+    class Obj
+    {
+        public void Display<T>(T param)
+        {
+            Console.WriteLine(param.ToString());
+        }
     }
 }
