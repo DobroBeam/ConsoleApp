c00ef8f5d12c5a75b202961d291a4d75ca380e0d
Dmitry Lazarenko <dobrobeam@gmail.com>
2022-04-20 11:36:13 +0300

    7.6.6

--- 7.6. Generic types/Program.cs
+++ 7.6. Generic types/Program.cs
@@ -20,4 +20,14 @@ namespace _7._6._Generic_types
     }
     class ElectricEngine { }
     class GasEngine { }
+
+//  Задание 7.6.6
+//  Реализуйте класс-обобщение Record, у которого будут два универсальных параметра:
+//  один — для идентификатора записи(Id), другой — для значения записи(Value). Также в классе реализуйте поле Date типа DateTime.
+    class Record<T1,T2>
+    {
+        public T1 Id;
+        public T2 Value;
+        public DateTime Date;
+    }
 }
